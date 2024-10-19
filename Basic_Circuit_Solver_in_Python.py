e09a21de9c458fdea25903f881814a4f658fc078
Dileepkumar M N <dileepmn29@gmail.com>
2024-10-19 17:43:59 +0530

    person5: changes made in code

--- Basic_Circuit_Solver_in_Python.py
+++ Basic_Circuit_Solver_in_Python.py
@@ -1,2 +1,13 @@
 #Basic_Circuit_Solver_in_Python
 #Write your code here
+
+
+class TestKVL(unittest.TestCase):
+
+    # Testing KVL function
+    def test_solve_kvl(self):
+        self.assertTrue(solve_kvl([10, -5, -5]))  # Should satisfy KVL
+        self.assertFalse(solve_kvl([10, -5, -3]))  # Should violate KVL
+
+# Running the KVL tests
+unittest.main(argv=[''], verbosity=2, exit=False)
